0b4518f42fb7d6da1d049352ec5c902f5500fd2e
arthurdecloedt <arthurdecloedt@hotmail.com>
2016-04-04 23:52:54 +0200

    extra Astar shit, still finding my way in this project

--- OGP1516-Hillbillies/src/hillbillies/model/activities/Astar.java
+++ OGP1516-Hillbillies/src/hillbillies/model/activities/Astar.java
@@ -2,38 +2,47 @@ package hillbillies.model.activities;
 
 
 import hillbillies.model.Unit;
-import hillbillies.model.VLocation;
+import hillbillies.model.activities.Cube;
 
 import java.util.*;
 
 public class Astar {
 
-    protected VLocation[] FindPath(Unit unit, VLocation startcube, VLocation destination) throws RuntimeException {
+
+    private Astar(int[] destination,Unit unit){
+        this.finaldestination=new Cube(destination);
+        this.unit=unit;
+    }
+    private final Cube finaldestination;
+
+    private final Unit unit;
+
+    protected Cube[] FindPath(Unit unit, Cube startcube, Cube destination) throws RuntimeException {
 
         //nodes already discovered and evaluated
-        Set<VLocation> closedSet= Collections.emptySet();
+        Set<Cube> closedSet= Collections.emptySet();
         //nodes already discovered but yet to be evaluated
-        Set<VLocation> openSet= Collections.emptySet();
+        Set<Cube> openSet= Collections.emptySet();
 
         openSet.add(startcube);
         //the node this node can be most efficiently be reached from
-        Map<VLocation,VLocation> cameFrom=new Map<VLocation,VLocation>(){};
+        Map<Cube,Cube> cameFrom=new HashMap<Cube,Cube>(){};
         // The cost of getting to that node from the start
-        Map<VLocation,Double> gScore=new Map<VLocation, Double>() {};
+        Map<Cube,Double> gScore=new HashMap<Cube, Double>() {};
         // distance from start to start is 0
         gScore.put(startcube,(double) 0);
 
         // A cost estimate for reaching the destination from the start through this node
         // partly known (start => node) partly heuristic (node=>destination)
-        Map<VLocation,Double> FScore=new Map<VLocation, Double>() {};
+        Map<Cube,Double> FScore=new HashMap<Cube, Double>() {};
         FScore.put(startcube,costEstimatefrom(startcube,destination));
 
 
         while (!openSet.isEmpty()){
-            VLocation current= startcube;
+            Cube current= startcube;
             double fscore=Double.MAX_VALUE;
             //search the discovered neighbour that is probably the closest to the destination
-            for (VLocation member:
+            for (Cube member:
                  openSet) {
                 double currentFScore=FScore.getOrDefault(member,Double.MAX_VALUE);
                 if (currentFScore<=fscore){
@@ -49,12 +58,12 @@ public class Astar {
             openSet.remove(current);
             closedSet.add(current);
             //alle buren worden ontdekt en er worden schattingen gemaakt van de afstand tot de bestemming
-            for (VLocation buur :
+            for (Cube buur :
                     getAllNeighbours(current)) {
                 if (closedSet.contains(current)){
                     continue;
                 }
-                double tentativeGScore=gScore.get(current)+VLocation.getStraightDist(current,buur);
+                double tentativeGScore=gScore.get(current)+Cube.getStraightDist(current,buur);
                 if (!openSet.contains(buur)){
                     openSet.add(buur);
                 }
@@ -75,18 +84,31 @@ public class Astar {
 
     }
 
-    private double costEstimatefrom(VLocation node,VLocation destination){
-        return VLocation.getStraightDist(node,destination);
+    private double costEstimatefrom(Cube node,Cube destination){
+        return Cube.getStraightDist(node,destination);
     }
-    private VLocation[] reconstructPath(Map<VLocation,VLocation> cameFrom,VLocation current){
-        List<VLocation> path= new ArrayList<VLocation>();
+    private Cube[] reconstructPath(Map<Cube,Cube> cameFrom,Cube current){
+        List<Cube> path= new ArrayList<Cube>();
         path.add(current);
 
         while (cameFrom.containsKey(current)){
             current=cameFrom.get(current);
             path.add(current);
         }
-        VLocation[] path2=new VLocation[path.size()];
+        Cube[] path2=new Cube[path.size()];
         return path.toArray(path2);
+    }
+
+    private Set<Cube> getAllNeighbours(Cube cube){
+        Iterable<Cube> intermediate=cube.generateNeighbours();
+        Set<Cube> set=Collections.EMPTY_SET;
+        for (Cube possibleCube:
+                intermediate) {
+            if (unit.getWorld().canHaveAsCubeLocation(possibleCube.locArray,unit)){
+                set.add(possibleCube);
+            }
+
+        }
+        return set;
     }
 }
