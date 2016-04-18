87eb2484713e18e8564d1b19a369cd13a137b727
Julie Allard <julie.allard@student.kuleuven.be>
2016-04-18 20:30:22 +0200

    tobby toert

--- OGP1516-Hillbillies/src/hillbillies/model/Scheduler.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Scheduler.java
@@ -5,35 +5,99 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ListIterator;
 
+/**
+ * A class of Schedulers.
+ * 
+ * @version 0.9 alpha
+ * @author  Arthur Decloedt - Bachelor in de Informatica
+ * 			Julie Allard - Bachelor Handelsingenieur in de beleidsinformatica  
+ * 			https://github.com/julieallard/HillBilliesOGP.git
+ */
 public class Scheduler {
 	
+	/**
+	 * Initialize this new Scheduler with given faction.
+     *
+     * @param  faction
+     *         The faction for this new Scheduler.
+     * @post   The faction of this new Scheduler is equal to the given faction.
+     */
 	public Scheduler(Faction faction) {
 		this.faction = faction;
 		this.TaskList = new ArrayList<>();
 	}
 	
+	/**
+	 * Variable registering the faction of this Scheduler.
+	 */
 	private final Faction faction;
 	
+	/**
+	 * Return the faction of this Scheduler.
+	 */
 	public Faction getFaction() {
 		return this.faction;
 	}
 	
+	/**
+	 * Add the given task to this Scheduler.
+	 * 
+	 * @param  task
+	 * 		   The task to add.
+	 * @post   The given task is added to this Scheduler.
+	 * @throws IllegalArgumentException
+	 * 		   The given task cannot have this Scheduler as a Scheduler.
+	 */
 	public void addTask(Task task) {
 		TaskList.add(task);
 	}
 	
+	/**
+	 * Add the given list of tasks to this Scheduler.
+	 * 
+	 * @param  addList
+	 * 		   The list of tasks to add.
+	 * @post   The given list of tasks is added to this Scheduler.
+	 * @throws IllegalArgumentException
+	 * 		   The given list of tasks cannot have this Scheduler as a Scheduler.
+	 */
 	public void addTask(List<Task> addList) {
 		TaskList.addAll(addList);
 	}
 	
+	/**
+	 * Remove the given task from this Scheduler.
+	 * 
+	 * @param  task
+	 * 		   The task to remove.
+	 * @post   The given task is removed from this Scheduler.
+	 */
 	public void removeTask(Task task) {
 		TaskList.remove(task);
 	}
 	
+	/**
+	 * Remove the given list of tasks from this Scheduler.
+	 * 
+	 * @param  removeList
+	 * 		   The list of tasks to remove.
+	 * @post   The given list of tasks is removed from this Scheduler.
+	 */
 	public void removeTask(List<Task> removeList) {
 		TaskList.removeAll(removeList);		
 	}
 	
+	/**
+	 * Replace the given task from this Scheduler by the given new task.
+	 * 
+	 * @param  oldTask
+	 * 		   The task to remove.
+	 * @param  newTask
+	 * 		   The task to add.
+	 * @post   The given new task is added to this Scheduler.
+	 * @effect The given old task is removed from this Scheduler.
+	 * @throws This Scheduler does not contain the given task to be replaced.
+	 */
 	public void replace(Task oldTask, Task newTask) throws IllegalArgumentException {
 		int index = TaskList.indexOf(oldTask);
 		if (index == -1)
@@ -81,12 +145,12 @@ public class Scheduler {
 		
 	}
 	
-	public void markAsScheduled(Task task) {
+	public void markExecution(Task task) {
 		
 	}
 	
 	public void resetMarking(Task task) {
-		
+		Unit.
 	}
 	
 	/**
@@ -94,7 +158,6 @@ public class Scheduler {
 	 * 
 	 * @invar The list of Tasks is effective.
 	 * @invar Each element in the list of Tasks references a Task that is an acceptable Task for this Scheduler.
-	 * @invar Each Task in the TaskList references this Scheduler as the Scheduler to which it is attached.
 	 */
 	private List<Task> TaskList;
 	
--- OGP1516-Hillbillies/src/hillbillies/model/Task.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Task.java
@@ -1,15 +1,133 @@
 package hillbillies.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import hillbillies.model.EsotERICScript.Statement;
+
+/**
+ * A class of Tasks.
+ * 
+ * @version 0.9 alpha
+ * @author  Arthur Decloedt - Bachelor in de Informatica
+ * 			Julie Allard - Bachelor Handelsingenieur in de beleidsinformatica  
+ * 			https://github.com/julieallard/HillBilliesOGP.git
+ */
 public class Task {
 	
+	/**
+	 * Initialize this new Task with given name, given priority and given scheduler.
+     *
+     * @param  name
+     *         The name for this new Task.
+     * @param  priority
+     * 		   The priority for this new Task.
+     * @param  scheduler
+     * 		   The scheduler for this new Task.
+     * @effect The given scheduler is added to this new Task.
+     * @post   The name of this new Task is equal to the given name.
+     * @post   The priority of this new Task is equal to the given priority.
+     */
+	public Task(String name, int priority, Scheduler scheduler) {
+		this.name = name;
+		this.priority = priority;
+		this.ActivityList = new ArrayList<>();
+		this.SchedulerSet = new HashSet<>();
+		addScheduler(scheduler);
+	}
+	
+	/**
+	 * Initialize this new Task with given name, given priority and given scheduler.
+     *
+     * @param  name
+     *         The name for this new Task.
+     * @param  priority
+     * 		   The priority for this new Task.
+     * @param  schedulerList
+     * 		   The list of schedulers for this new Task.
+     * @effect The given list of schedulers is added to this new Task.
+     * @post   The name of this new Task is equal to the given name.
+     * @post   The priority of this new Task is equal to the given priority.
+     */
+	public Task(String name, int priority, List<Scheduler> schedulerList) {
+		this.name = name;
+		this.priority = priority;
+		this.ActivityList = new ArrayList<>();
+		this.SchedulerSet = new HashSet<>();
+		addScheduler(schedulerList);
+	}
+	
+	/**
+	 * Variable registering the name of this Task.
+	 */
+	private String name;
+	
+	/**
+	 * Variable registering the priority of this Task.
+	 */
+	private int priority;
+	
+	/**
+	 * Variable registering whether the of this Task.
+	 */
 	private boolean isBeingExecuted = false;
+	
+	/**
+	 * Variable registering the executing unit of this Task.
+	 */
+	private Unit executer;
+	
+	/**
+	 * List collecting references to activities belonging to this Task.
+	 * 
+	 * @invar The list of activities is effective.
+	 * @invar Each element in the list of activities references an activity that is an acceptable activity for this Task.
+	 * @invar Each activity in the ActivityList references this Task as the Task to which it is attached.
+	 */
+	private List<Statement> ActivityList;
+	
+	/**
+	 * List collecting references to schedulers belonging to this Task.
+	 * 
+	 * @invar The list of schedulers is effective.
+	 * @invar Each element in the list of schedulers references a scheduler that is an acceptable scheduler for this Task.
+	 */
+	private Set<Scheduler> SchedulerSet;
 	
 	public boolean isBeingExecuted() {
 		return this.isBeingExecuted;
 	}
 	
-	public void stop() {
-		
+	public void addScheduler(Scheduler scheduler) {
+		this.SchedulerSet.add(scheduler);
+	}
+	
+	public void addScheduler(List<Scheduler> schedulerList) {
+		this.SchedulerList.addAll(schedulerList);
+	}
+	
+    /**
+     * Set the state of execution of this Task according to the given flag.
+     *
+     * @param  flag
+     * 		   The execution state to be registered.
+     * @post   The new execution state of this Task is equal to the given flag.
+     * 		 | this.isBeingExecuted == flag
+     */
+    private void setExecution(boolean flag) {
+        this.isBeingExecuted = flag;
+    }
+	
+	public void startExecution(Unit unit) {
+		this.executer = unit;
+		setExecution(true);
+	}
+	
+	public void stopExecution() {
+		this.executer = null;
+		setExecution(false);
 	}
 	
 	
