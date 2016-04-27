e41fcbaef9def3956401e1dd111b71c92950235e
arthurdecloedt <arthurdecloedt@hotmail.com>
2016-04-27 10:46:46 +0200

    you must pull the commits to pass

--- OGP1516-Hillbillies/src/hillbillies/model/EsotERICScript/Statement.java
+++ OGP1516-Hillbillies/src/hillbillies/model/EsotERICScript/Statement.java
@@ -10,22 +10,26 @@ import hillbillies.model.exceptions.SyntaxError;
 
 public  class Statement {
 
-    public Statement(Expression exarg1,Expression exarg2)
+    public Statement(Object ... args){
 
+    }
     public void Execute(Unit unit) throws SyntaxError{
         this.partStatement.execute(unit);
     }
     private PartStatement partStatement;
 
-    public class WorkPartStatement extends PartStatement{
+
+    class WorkPartStatement extends PartStatement{
 
         public WorkPartStatement(PositionExpression argument) {
             this.argExpr1=argument;
         }
-        public PositionExpression argExpr1;
+        PositionExpression argExpr1;
         @Override
         public void execute(Unit unit) throws SyntaxError {
             unit.work(argExpr1.value());
             this.setExecuted(true);
     }
-}
+
+
+}}
