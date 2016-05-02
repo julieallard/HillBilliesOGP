8d83e8156757524a0afcf60eaea0ec9ab65fd805
arthurdecloedt <arthurdecloedt@hotmail.com>
2016-05-02 12:22:59 +0200

    Triple J on the road to Suplex City!!!

--- OGP1516-Hillbillies/src/hillbillies/model/ProgramExecutor.java
+++ OGP1516-Hillbillies/src/hillbillies/model/ProgramExecutor.java
@@ -1,14 +1,49 @@
 package hillbillies.model;
 
-/**
- * Created by arthurdecloedt on 2/05/16.
- */
+import hillbillies.model.EsotERICScript.Statements.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
 public class ProgramExecutor {
 
+    private final Unit Excecutor;
+    private final Task task;
+    private final List<Statement> statementList;
 
-    
+    private Stack<Statement> statementStack;
+
+    public ProgramExecutor(Unit excecutor, Task task, List<Statement> statementList) {
+        this.Excecutor = excecutor;
+        this.task = task;
+        this.statementList=new ArrayList<>();
+        this.statementStack=new Stack<>();
+
+    }
+
+    public Unit getExcecutor() {
+        return Excecutor;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public List<Statement> getStatementList() {
+        return statementList;
+    }
+
+    public Stack<Statement> getStatementStackClone() {
+        return (Stack<Statement>) statementStack.clone();
+    }
 
 
 
 
+    public static boolean TaskBreakValidityCheck(Task task){
+
+        task.taks
+    }
 }
